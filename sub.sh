db2387fefddbe8db385f7973f4bfa80fc987aff0
navya <jenkins@123gmail.com>
2024-08-28 16:23:04 +0530

    hihello

---
+++ sub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:9 "
+
+
+# Ask user for second number
+echo "Enter second number:3 "
+
+
+# Perform subtraction
+diff=$((9 - 3))
+
+# Display result
+echo "Difference: $diff"
